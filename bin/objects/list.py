3c19a456be03265b533b709b23e5433e3f423b13
SartoRiccardo <rikki.sarto@gmail.com>
2019-04-10 09:42:06 +0200

    Oggetto Lista

--- bin/objects/list.py
+++ bin/objects/list.py
@@ -1,13 +1,35 @@
-from tkinter import Listbox, END, EXTENDED, BOTH
+from tkinter import Listbox, END, SINGLE
 
 
 class List:
     def __init__(self, root, array=[]):
         self.__root = root
 
-        self.__list = Listbox(self.__root, selectmode=EXTENDED)
+        self.__list = Listbox(self.__root, selectmode=SINGLE)
         self.__list.config(width=40)
-        self.__list.pack(fill=BOTH, expand=1)
+        self.__list.pack()
 
+        self.__lessons = [x for x in array]
         for item in array:
             self.__list.insert(END, str(item))
+
+    def delete(self, i):
+        if 0 <= i < len(self.__lessons):
+            self.__list.delete(i)
+            self.__lessons.pop(i)
+
+    def add(self, lesson, i=None):
+        if i is None:
+            i = len(self.__lessons)
+        self.__list.insert(i, str(lesson))
+        self.__lessons.insert(i, lesson)
+
+    def getSelectedElement(self):
+        if self.__list.curselection() is ():
+            return None
+        else:
+            return self.__lessons[self.__list.curselection()[0]]
+
+    def setSelectedItem(self, i):
+        self.__list.activate(i)
+
