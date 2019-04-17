49f05cae194c661e3d37cc19dc1e286f652f925d
Stini01 <enri.braga@gmail.com>
2019-04-17 09:39:15 +0200

    Aggiunta di una nuova lezione: sistemata view.

--- bin/objects/list.py
+++ bin/objects/list.py
@@ -1,17 +1,21 @@
-from tkinter import Listbox, END, SINGLE
+from tkinter import *
 
 
 class List:
     def __init__(self, root, array=[]):
         self.__root = root
 
-        self.__list = Listbox(self.__root, selectmode=SINGLE)
+        self.__scrollBar = Scrollbar(self.__root)
+        self.__scrollBar.pack(side=RIGHT, fill=Y)
+
+        self.__list = Listbox(self.__root, selectmode=SINGLE, yscrollcommand=self.__scrollBar.set)
         self.__list.config(width=40)
         self.__list.pack()
-
         self.__lessons = [x for x in array]
         for item in array:
             self.__list.insert(END, str(item))
+
+        self.__scrollBar.config(command=self.__list.yview)
 
     def getList(self):
         return self.__list
